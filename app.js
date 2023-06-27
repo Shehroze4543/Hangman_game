84fbfb434fa68abccc37ef253f92dab692f87423
Shehroze4543 <Shehroze4543@gmail.com>
2023-06-26 20:08:00 -0400

    my first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// ARRAY OF WORDS
+
+// A function that would pick a random word from the above array
+
+// A counter variable for the wrong guess
+
+//  a variable that keeps track of the maximum number of wrong guesses that are allowed.
+
+// function to see if the letter that is guessed is right or wrong
+
+// function to update the hangman images when the player makes a wrong guess
+
+// function to change the secret word by adding the letter that was guessed correctly.
+
+// function to display game over when the player reaches the max number of wrong guesses
+
+// function to reset the game whenever the player presses the RESET BUTTON
+
+// function  to switch player from player 1 to player 2
+
+// function to calculte both player 1 and player 2 scores indivually
+
+//function to compare both scores and determine the winner
+
+// a function to restart the game
